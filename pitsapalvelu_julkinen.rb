ad56d1ec152b992b3bde9795cbbc42b542b7118b
Santtu Keskinen <laquendi@gmail.com>
2011-08-22 01:25:24 +0300

    Tilaaminen ja tilauksen seuranta

--- pitsapalvelu_julkinen.rb
+++ pitsapalvelu_julkinen.rb
@@ -13,7 +13,7 @@ class Pitsapalvelu_julkinen < Sinatra::Base
   $db.results_as_hash = true
 
   def tyhjenna_kori
-    response.set_cookie("kori", {:value=>'', :path=>'/',})
+    response.set_cookie("kori", {:value=>'', :path=>'/'})
   end
 
   def tarkista_kori
@@ -30,13 +30,13 @@ class Pitsapalvelu_julkinen < Sinatra::Base
     indeksi = 0
     sisalto.each { |i|
       idt = i.split('a')
-      tuote = $db.get_first_row("select t.nimi, t.id, tt.lisukkeet from tuote t, tuotetyyppi tt where tt.id=t.tuotetyyppi_id and t.id=?;", idt[0])
-      hinta = $db.get_first_row("select saatavilla, hinta from tuote_hinnat where tuote=? and alku<? and loppu>? limit 1;", idt[0], aika, aika)
+      tuote = $db.get_first_row("select t.nimi, t.id, tt.id as lista_id, tt.lisukkeet from tuote t, tuotetyyppi tt where tt.id=t.tuotetyyppi_id and t.id=?;", idt[0])
+      hinta = $db.get_first_row("select saatavilla, hinta, lista from tuote_hinnat where tuote=? and alku<? and loppu>? limit 1;", idt[0], aika, aika)
       if !tuote or !hinta or !hinta['saatavilla']
         next
       end
       lopullinen_sisalto << idt.shift.to_s
-      tuote_hash = {:indeksi=>indeksi, :id=>tuote['id'].to_i, :nimi=>tuote['nimi'], :hinta=>hinta['hinta'], :onko_lisukkeet=>tuote['lisukkeet']}
+      tuote_hash = {:indeksi=>indeksi, :id=>tuote['id'].to_i, :lista=>hinta['lista'], :nimi=>tuote['nimi'], :hinta=>hinta['hinta'], :onko_lisukkeet=>tuote['lisukkeet']}
       tuote_lisukkeet=[]
       if tuote['lisukkeet']
         idt.each{ |lisuke_id|
@@ -99,32 +99,157 @@ class Pitsapalvelu_julkinen < Sinatra::Base
     $db.execute("select * from lisuke").each { |i|
       @lisukkeet[i['id']]=i['nimi']
     }
+    @summa = @sisalto.reduce(0.0) {|memo, obj| memo+obj[:hinta]}
     haml :kori
   end
   get '/kori/tyhjennys' do
     tyhjenna_kori
     redirect "/kori"
   end
-  post '/kori/lisaa_lisuke/:tuote_indeksi' do
-    @sisalto = tarkista_kori
-
-    lisuke = $db.get_first_value("select id from lisuke where nimi=?",params[:lisuke])
-    unless lisuke.nil?
-      @sisalto[params[:tuote_indeksi].to_i][:lisukkeet].push(lisuke)
-      keksi = ""
-      @sisalto.each { |i|
-        keksi << i[:id].to_s
-        i[:lisukkeet].each { |p|
-          keksi << "a#{p}"
-        }
-        keksi << "b"
+  get '/kori/poista/:tuote_indeksi' do
+    sisalto = tarkista_kori
+    sisalto.delete_at(params[:tuote_indeksi].to_i)
+    keksi = ""
+    sisalto.each { |i|
+      keksi << i[:id].to_s
+      i[:lisukkeet].each { |p|
+        keksi << "a#{p}"
       }
-      response.set_cookie("kori", {:value=>keksi, :path=>'/'})
-    end
+      keksi << "b"
+    }
+    response.set_cookie("kori", {:value=>keksi, :path=>'/'})
+    redirect '/kori'
+  end
+  get '/tilaa' do
+    aika = Time.now+(60*35)
+    @aika = "#{aika.hour.to_s.rjust(2,'0')}:#{aika.min.to_s.rjust(2,'0')}"
+    @sisalto = tarkista_kori
+    @summa = @sisalto.reduce(0.0) {|memo, obj| memo+obj[:hinta]}
     @lisukkeet = {}
     $db.execute("select * from lisuke").each { |i|
       @lisukkeet[i['id']]=i['nimi']
+	}	
+    haml :tilaa
+  end
+  get '/seuranta' do
+    tilaus_id = session[:tilaus]
+    tilaus_id ||= -1
+    @tilaus = $db.get_first_row("select id, tila, nimi, osoite, toimitusaika from tilaus where id=?;", tilaus_id)
+    unless @tilaus
+      return haml :ei_seurattavaa
+    end
+    @tila = $db.get_first_row("select * from tilat where id=?;", @tilaus['tila'])
+    @tuotteet = $db.execute("select * from ostos where tilaus_id=?;", tilaus_id)
+    @summa = $db.get_first_value("select sum(hinta) from ostos where tilaus_id=?;", tilaus_id)
+    @tuote_idt = {}
+    $db.execute("select * from tuote;").each { |tuote|
+      @tuote_idt[tuote['id']]=tuote['nimi']
     }
-    haml :kori
+    aika = Time.now.to_a
+    @tilaus['toimitusaika'] =~ /(\d\d):(\d\d)/
+    aika[2]-=$1.to_i
+    aika[1]-=$2.to_i
+    @erotus = -60*aika[2]-aika[1]
+    @tila = $db.get_first_row("select * from tilat where id=?;", @tilaus['tila'])
+    haml :seuranta
+  end
+  get '/peru' do
+      tilaus_id = session[:tilaus]
+      tilaus_id ||= -1
+      @tilaus = $db.get_first_row("select toimitusaika from tilaus where id=?;", tilaus_id)
+      if !@tilaus
+        return haml :peru
+      end
+      aika = Time.now.to_a
+      @tilaus['toimitusaika'] =~ /(\d\d):(\d\d)/
+      aika[2]-=$1.to_i
+      aika[1]-=$2.to_i
+      @erotus = -60*aika[2]-aika[1]
+      if @erotus < 30
+        return haml :peru
+      end
+        $db.execute("update tilaus set tila=? where id=?;", 2, tilaus_id)
+      haml :peru
+  end
+
+  post '/kori/lisaa_lisuke/:tuote_indeksi' do
+      sisalto = tarkista_kori
+
+      lisuke = $db.get_first_value("select id from lisuke where nimi=?",params[:lisuke])
+      unless lisuke.nil?
+          sisalto[params[:tuote_indeksi].to_i][:lisukkeet].push(lisuke)
+          keksi = ""
+          sisalto.each { |i|
+              keksi << i[:id].to_s
+              i[:lisukkeet].each { |p|
+                  keksi << "a#{p}"
+              }
+              keksi << "b"
+          }
+          response.set_cookie("kori", {:value=>keksi, :path=>'/'})
+      end
+      redirect '/kori'
+  end
+  post '/tilaa' do
+      sisalto = tarkista_kori
+      if sisalto.empty?
+          @virhe = "Kori on tyhja"
+          return haml :tilaus_epaonnistui
+      end
+      if params[:toimitus] == '0'
+          kotiinkuljetus = 0
+      elsif params[:toimitus] == '1'
+          kotiinkuljetus = 1
+      else
+          @virhe = "Valitse joko nouto tai kotiinkuljetus"
+          return haml :tilaus_epaonnistui
+      end
+      if params[:tilaaja_nimi].match(/\A[\w\s]+\Z/)
+          tilaaja_nimi = params[:tilaaja_nimi]
+      else
+          @virhe = "Anna oikea nimi"
+          return haml :tilaus_epaonnistui
+      end
+      if (kotiinkuljetus and params[:tilaaja_osoite].match(/\A[\w\s]+\Z/)) or params[:tilaaja_osoite].match(/\A[\w\s]*\Z/)
+          tilaaja_osoite = params[:tilaaja_osoite]
+      else
+          @virhe = "Kotiinkuljetuksessa taytyy antaa osoite"
+          return haml :tilaus_epaonnistui
+      end
+      if params[:tilaaja_puhelin].match(/\A[+\d\s]{6,}\Z/)
+          tilaaja_puhelin = params[:tilaaja_puhelin]
+      else
+          @virhe = "Anna oikea puhelinnumero"
+          return haml :tilaus_epaonnistui
+      end
+    if params[:toimitusaika].match(/\A(\d\d):(\d\d)\Z/) 
+      aika = Time.now.to_a
+      aika[2]-=$1.to_i
+      aika[1]-=$2.to_i
+      erotus = -60*aika[2]-aika[1]
+      if erotus >= 30
+        toimitusaika = params[:toimitusaika]
+      else
+        @virhe = "Toimitus aikaisintaan 30min paasta"
+        return haml :tilaus_epaonnistui
+      end
+    else
+      @virhe = "Aika muodossa xx:xx. Toimitus aikaisintaan 30min paasta"
+      return haml :tilaus_epaonnistui
+    end
+    lisatiedot = params[:lisatiedot]
+    $db.execute("insert into tilaus(kotiinkuljetus,nimi,osoite,puhelin,toimitusaika,lisatiedot,tila,tilausaika) values(?,?,?,?,?,?,?,strftime('%s','now'));",
+              kotiinkuljetus, tilaaja_nimi, tilaaja_osoite, tilaaja_puhelin, toimitusaika, lisatiedot, 1);
+    id = $db.last_insert_row_id()
+    sisalto.each { |tuote|
+      $db.execute("insert into ostos(tilaus_id, tuote_id, ruokalista_id, hinta) values(?,?,?,?);", id, tuote[:id], tuote[:lista], tuote[:hinta])
+      tuote_id = $db.last_insert_row_id()
+      tuote[:lisukkeet].each { |lisuke|
+        $db.execute("insert into lisuke_ostos(lisuke_id, ostos_id) values(?,?);", lisuke, tuote_id)
+      }
+    }
+    session[:tilaus]=id
+    tyhjenna_kori
+    redirect '/seuranta'
   end
 end
